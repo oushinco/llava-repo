aca688f35af6fe012d035485d3f280a8867a9f50
Xinguang Wang <wang1xin@police.nsw.gov.au>
2023-11-23 16:42:38 +1100

    start

--- start_service.sh
+++ start_service.sh
@@ -15,9 +15,9 @@ python -m llava.serve.gradio_web_server --controller http://localhost:10000 --mo
 sleep 15
 
 # Start the model worker
-# python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-7b &
+# python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-7b --load-8bit &
 
-python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-13b  --load-8bit &
+python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path liuhaotian/llava-v1.5-13b &
 
 # Keep the script running to keep the container alive
 tail -f /dev/null
